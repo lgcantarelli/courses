e5a6264102aedf809081e02bbc4fa8ddd8722536
Lucas Guerreiro Cantarelli <lgcantarelli@gmail.com>
2022-12-11 15:54:52 -0300

    feat(react-fundamentals): username form with ref

--- epic-react/react-fundamentals/src/exercise/06.js
+++ epic-react/react-fundamentals/src/exercise/06.js
@@ -4,6 +4,12 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameRef = React.useRef()
+
+  function handleSubmit(event) {
+    onSubmitUsername(usernameRef.current.value)
+    event.preventDefault()
+  }
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
@@ -20,10 +26,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input ref={usernameRef} type="text" id="username"/>
       </div>
       <button type="submit">Submit</button>
     </form>
