893fcbd3fab28691ce5a24b00e473099a8a148b8
Lucas Guerreiro Cantarelli <lgcantarelli@gmail.com>
2022-12-26 15:47:27 -0300

    feat(react-hooks/04-ec): tic tac toe with version history and persisted

---
+++ react-hooks/src/exercise/04.extra-3.js
@@ -0,0 +1,164 @@
+// useState: tic tac toe
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import { useLocalStorageState } from '../utils'
+
+function Board({ squares, onSelectSquare }) {
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => onSelectSquare(i)}>
+        {squares[i]}
+      </button>
+    )
+  }
+
+  return (
+    <div className="game-board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+function BoardControlPanel({ status, history, currentStep, onSelectStep }) {
+  function renderMove(move) {
+    const isCurrentMove = move === currentStep
+
+    function buttonLabel() {
+      const currentLabel = isCurrentMove ? '(current)' : ''
+
+      return move === 0
+        ? `Go to game start ${currentLabel}`
+        : `Go to move #${move} ${currentLabel}`
+    }
+
+    return (
+      <li key={move}>
+        <button
+          onClick={() => onSelectStep(move)}
+          disabled={isCurrentMove}
+        >
+          {buttonLabel()}
+        </button>
+      </li>
+    )
+  }
+
+  return (
+    <div className="board-control-panel">
+      <div className="status">{status}</div>
+      <ol>
+        {history.map((_, i) => renderMove(i))}
+      </ol>
+    </div>
+  )
+}
+
+function Game() {
+  const [history, setHistory] = useLocalStorageState('tic-tac-toe:history', [Array(9).fill(null)])
+  const [currentStep, setCurrentStep] = useLocalStorageState('tic-tac-toe:step', 0)
+
+  const currentSquares = React.useMemo(
+    () => history[currentStep], [history, currentStep]
+  )
+
+  const nextValue = calculateNextValue(currentSquares)
+  const winner    = calculateWinner(currentSquares)
+  const status    = calculateStatus(winner, currentSquares, nextValue)
+
+  function selectSquare(square) {
+    const value = currentSquares[square]
+
+    if (value || winner) return
+
+    const squaresCopy = [...currentSquares]
+    squaresCopy[square] = nextValue
+    
+    setHistory([...history, squaresCopy])
+    setCurrentStep(step => step + 1)
+  }
+
+  function selectStep(nextStep) {
+    setCurrentStep(nextStep)
+  }
+
+  function restart() {
+    setHistory([Array(9).fill(null)])
+    setCurrentStep(0)
+  }
+
+  return (
+    <div className="game">
+      <div>
+        <Board
+          squares={currentSquares}
+          onSelectSquare={selectSquare}
+        />
+
+        <button className="restart" onClick={restart}>restart</button>
+      </div>
+
+      <BoardControlPanel
+        status={status}
+        history={history}
+        currentStep={currentStep}
+        onSelectStep={selectStep}
+      />
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function App() {
+  return <Game />
+}
+
+export default App
